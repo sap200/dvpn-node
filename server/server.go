ffe63703f084f0ad067bc02ffaf090e2c4815190
saptarsi <saptarsi2000@outlook.com>
2021-09-08 14:29:34 +0530

    added stub for launching server

--- server/server.go
+++ server/server.go
@@ -1,1 +1,12 @@
 package server
+
+// launches a server to listen to incoming requests and deliver the responses accordingly
+
+// this done in handleConnection
+// in case of intiated message it sends the pubkey
+// in case of terminated message it revokes the client access and deletes the public key.
+
+// Aashin's part..
+func LaunchServer() {
+
+}
