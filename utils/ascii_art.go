564f3e74d2c8e45b76fbe1ca3cc1555b16acd332
saptarsi <saptarsi2000@outlook.com>
2021-09-24 16:56:45 +0530

    added ascii art functionality

--- utils/ascii_art.go
+++ utils/ascii_art.go
@@ -6,6 +6,9 @@ import (
 	figure "github.com/common-nighthawk/go-figure"
 )
 
+// ascii_art creates ascii art for different sentences.
+// this makes the node looks better, nice and professional
+
 // PrintServer does ascii art for the string server
 func PrintServer() {
 	myFigure := figure.NewFigure("Zovino Server", "", true)
